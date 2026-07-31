c8017c7ecf95623fe932305bf7a22be6fa73fe5a
code9HARSHIT <therealharshit9@gmail.com>
2026-07-31 07:11:36 +0000

    java

--- lab2/JaggedArray.java
+++ lab2/JaggedArray.java
@@ -1,18 +1,31 @@
 package lab2;
-
+import java.util.Scanner;
 public class JaggedArray {
     public static void main(String[] args) {
-        int[][] ja = new int[3][];
-        ja[0] = new int[]{1, 2, 3,5,8};
-        ja[1] = new int[]{4, 77,5};
-        ja[2] = new int[]{6, 7, 8, 9,12,33,4,55,4};
+        Scanner s = new Scanner(System.in);
+        IO.println("Enter the number of rows:");
+        int row = s.nextInt();
+        int[][] ja = new int[row][];
+
+        
 
         for (int i = 0; i < ja.length; i++) {
+            IO.println("Enter the number of columns for row " + (i + 1) + ":");
+            int col = s.nextInt();
+            ja[i] = new int[col];
+            IO.println("Enter the elements for row " + (i + 1) + ":");
+            for (int j = 0; j < col; j++) {
+                ja[i][j] = s.nextInt();
+            }
+            
+        }
+        
+        for (int i = 0; i < ja.length; i++) {
             for (int j = 0; j < ja[i].length; j++) {
-                System.out.print(ja[i][j] + " ");
+                System.out.print(ja[i][j]+" ");
             }
             System.out.println();
-        }
+        }    
     }
     
 }
