3d80206116ecb8700e42dddd6b3011bf286bfeaa
code9HARSHIT <therealharshit9@gmail.com>
2026-08-23 09:52:45 +0000

    pr

---
+++ Projects/Valid.java
@@ -0,0 +1,56 @@
+package Projects;
+
+import java.util.Scanner;
+class Valid{
+    public  void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Valid i = new Valid();
+
+        System.out.println("===========================================================");
+        System.out.println("---------------#-#-#-CSM - DEPARTMENT-#-#-#----------------");
+        System.err.println("===========================================================");
+
+        System.out.print("Enter your name: ");
+        String name = s.nextLine();
+
+        System.out.print("Enter your roll number: ");
+        String roll = s.nextLine();
+        i.rollValid(roll);
+        
+        System.out.print("Enter your email: ");
+        String email = s.nextLine();
+        i.emailValid(email);
+
+        s.close();
+
+
+    }
+    public void rollValid(String roll){
+
+        if(roll.contains("AG1A") && roll.length() == 10){
+            if(roll.charAt(6) == '6' && roll.charAt(7) == '6'){
+                int a = 22;
+
+            }
+            else{
+            System.out.println("Roll number is invalid");
+            System.exit(0);
+        }
+
+        }
+        else{
+            System.out.println("Roll number is invalid");
+            System.exit(0);
+        }
+
+
+    }
+    public void emailValid(String email){
+        if(email.contains("@") && email.contains(".") && email.length() >= 4){
+            System.out.println("*-----Data Stored-----*");
+        }else{
+            System.out.println("Email is invalid");
+        }
+    }
+
+}
--- lab2/JaggedArray.java
+++ lab2/JaggedArray.java
@@ -2,30 +2,31 @@ package lab2;
 import java.util.Scanner;
 public class JaggedArray {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        IO.println("Enter the number of rows:");
-        int row = s.nextInt();
-        int[][] ja = new int[row][];
-
-        
-
-        for (int i = 0; i < ja.length; i++) {
-            IO.println("Enter the number of columns for row " + (i + 1) + ":");
-            int col = s.nextInt();
-            ja[i] = new int[col];
-            IO.println("Enter the elements for row " + (i + 1) + ":");
-            for (int j = 0; j < col; j++) {
-                ja[i][j] = s.nextInt();
+        try (Scanner s = new Scanner(System.in)) {
+            IO.println("Enter the number of rows:");
+            int row = s.nextInt();
+            int[][] ja = new int[row][];
+            
+            
+            
+            for (int i = 0; i < ja.length; i++) {
+                IO.println("Enter the number of columns for row " + (i + 1) + ":");
+                int col = s.nextInt();
+                ja[i] = new int[col];
+                IO.println("Enter the elements for row " + (i + 1) + ":");
+                for (int j = 0; j < col; j++) {
+                    ja[i][j] = s.nextInt();
+                }
+                
             }
             
-        }
-        
-        for (int[] rowArr : ja) {
-            for (int val : rowArr) {
-                System.out.print(val + " ");
+            for (int[] rowArr : ja) {
+                for (int val : rowArr) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }    
+        }   
     }
     
 }
