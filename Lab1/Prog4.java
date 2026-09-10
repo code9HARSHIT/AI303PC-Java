ec3322e708cadac4e76d5f60653fe5c4088704c3
Mr.H <therealharshit9@gmail.com>
2026-09-10 11:34:54 +0530

    Fix output formatting and uncomment input prompt

--- Lab1/Prog4.java
+++ Lab1/Prog4.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 public class Prog4 {
     public static void main(String[] args) {
         int[] x = new int[5];
-        //System.out.println("Enter the values:")
+        System.out.println("Enter the values:");
         Scanner s = new Scanner(System.in);
         for(int i=0;i<x.length;i++){
             x[i]=s.nextInt();
         }
-       System.out.println("The values are");
+       System.out.println("The values are: ");
         for(int v : x){
-            System.out.println(v);
+            System.out.print(v+" ");
         }
     }
     
 }
+
+/*
+    Sample output:
+    Enter the values:
+    1
+    2
+    3
+    4
+    5
+    The values are: 1 2 3 4 5
+*/
